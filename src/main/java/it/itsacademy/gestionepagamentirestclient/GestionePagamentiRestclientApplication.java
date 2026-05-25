9ed9d0a83aa1ea2132ea7e2ebf8d01f9408a78d9
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-25 10:45:51 +0200

    Initialized project

---
+++ src/main/java/it/itsacademy/gestionepagamentirestclient/GestionePagamentiRestclientApplication.java
@@ -0,0 +1,13 @@
+package it.itsacademy.gestionepagamentirestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionePagamentiRestclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionePagamentiRestclientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/itsacademy/gestionepagamentirestclient/GestionePagamentiRestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itsacademy.gestionepagamentirestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionePagamentiRestclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
