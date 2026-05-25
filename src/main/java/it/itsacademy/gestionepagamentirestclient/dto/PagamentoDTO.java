6ca40007dc66e53f7b1d0d617d4a6c8fbb47f8d3
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-25 15:20:23 +0200

    FEAT: Added more attributes to Pagamento

--- src/main/java/it/itsacademy/gestionepagamentirestclient/dto/PagamentoDTO.java
+++ src/main/java/it/itsacademy/gestionepagamentirestclient/dto/PagamentoDTO.java
@@ -3,6 +3,7 @@ package it.itsacademy.gestionepagamentirestclient.dto;
 import it.itsacademy.gestionepagamentirestclient.model.Pagamento;
 import lombok.*;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
@@ -10,6 +11,9 @@ import java.util.UUID;
 public class PagamentoDTO {
     private UUID idPagamento;
 
+    private UUID idOrdine;
+    private Double totale;
+    private LocalDate dataPagamento = LocalDate.now();
     private StatoPagamento statoPagamento;
 
     public enum StatoPagamento {
