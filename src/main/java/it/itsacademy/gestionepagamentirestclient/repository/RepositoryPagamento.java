e0d9a5e2052d46c97043beebce5dce14f3cf55f5
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-26 09:10:16 +0200

    REFACTOR: Now uses an enumarated of type string

--- src/main/java/it/itsacademy/gestionepagamentirestclient/repository/RepositoryPagamento.java
+++ src/main/java/it/itsacademy/gestionepagamentirestclient/repository/RepositoryPagamento.java
@@ -1,9 +1,14 @@
 package it.itsacademy.gestionepagamentirestclient.repository;
 
 import it.itsacademy.gestionepagamentirestclient.model.Pagamento;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.UUID;
 
 public interface RepositoryPagamento extends JpaRepository<Pagamento, UUID> {
+    @Enumerated(EnumType.STRING)
+    Collection<Pagamento> findByIdOrdine(UUID idOrdine);
 }
