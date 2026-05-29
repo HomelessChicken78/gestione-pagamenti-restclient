4ea750382c579e02529a0145149fd49515806f1a
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-29 14:50:24 +0200

    FEAT: Added method in repository to search all the payments with statoPagamento = RIFIUTATO

--- src/main/java/it/itsacademy/gestionepagamentirestclient/repository/RepositoryPagamento.java
+++ src/main/java/it/itsacademy/gestionepagamentirestclient/repository/RepositoryPagamento.java
@@ -2,10 +2,14 @@ package it.itsacademy.gestionepagamentirestclient.repository;
 
 import it.itsacademy.gestionepagamentirestclient.model.Pagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Collection;
 import java.util.UUID;
 
 public interface RepositoryPagamento extends JpaRepository<Pagamento, UUID> {
     Collection<Pagamento> findByIdOrdine(UUID idOrdine);
+
+    @Query("SELECT p FROM Pagamento AS p WHERE p.statoPagamento = 'RIFIUTATO'")
+    Collection<Pagamento> findWhereStatoRifiutato();
 }
